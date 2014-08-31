05b4e2e6cb9253bfc3f01b52f9abb7884eceabb4
tbmc <me@tbmc.fr>
2014-08-31 20:01:38 +0200

    Compression de données

--- compress.c
+++ compress.c
@@ -1,1 +1,72 @@
+#include "compress.h"
+
+// zlib
+#include "miniz.c"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int compression_level = -1;
+
+void setCompressionLevel(int level)
+{
+    if(!(-1 <= level && level <= 10))
+        return;
+    compression_level = level;
+}
+
+void compress_char_array(const char *in, uInt inSize,
+        char *out, uInt outMaxSize, uInt *outSize)
+{
+    z_stream defstream;
+    defstream.zalloc = Z_NULL;
+    defstream.zfree = Z_NULL;
+    defstream.opaque = Z_NULL;
+
+    defstream.avail_in = (uInt) inSize;
+    defstream.next_in = (Bytef *)in;
+    defstream.avail_out = (uInt) outMaxSize;
+    defstream.next_out = (Bytef *) out;
+
+    deflateInit(&defstream, compression_level);
+    deflate(&defstream, Z_FINISH);
+    deflateEnd(&defstream);
+
+    *outSize = defstream.total_out;
+}
+
+void uncompress_char_array(const char *in, uInt inSize,
+        char *out, uInt outMaxSize, uInt *outSize)
+{
+    z_stream infstream;
+    infstream.zalloc = Z_NULL;
+    infstream.zfree = Z_NULL;
+    infstream.opaque = Z_NULL;
+
+    infstream.avail_in = (uInt) inSize;
+    infstream.next_in = (Bytef *) in;
+    infstream.avail_out = (uInt) outMaxSize;
+    infstream.next_out = (Bytef *) out;
+
+    inflateInit(&infstream);
+    inflate(&infstream, Z_NO_FLUSH);
+    inflateEnd(&infstream);
+
+    *outSize = infstream.total_out;
+}
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
 
--- compress.h
+++ compress.h
@@ -1,4 +1,23 @@
 #ifndef COMPRESS_H
 #define COMPRESS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned int uInt;
+
+void setCompressionLevel(int level);
+
+void compress_char_array(const char *in, uInt inSize,
+              char *out, uInt outMaxSize, uInt *outSize);
+
+void uncompress_char_array(const char *in, uInt inSize,
+              char *out, uInt outMaxSize, uInt *outSize);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif // COMPRESS_H
