c6c25f2e6c26284401b87d66721190b5e7466c19
tbmc <me@tbmc.fr>
2014-09-01 17:18:57 +0200

    troll

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-#include <SFML/System.hpp>
 #include <iostream>
-
-void func()
-{
-    // cette fonction démarre lorsque thread.launch() est appelé
-
-    for (int i = 0; i < 10; ++i)
-        std::cout << "I'm thread number one" << std::endl;
-}
 
 int main()
 {
-    // crée un thread avec la fonction func() comme point d'entrée
-    sf::Thread thread(&func);
 
-    // exécute le thread
-    thread.launch();
-
-    // le thread principal continue à tourner...
-
-    for (int i = 0; i < 10; ++i)
-        std::cout << "I'm the main thread" << std::endl;
+#error TROLLLLLLLL!!!!
 
     return 0;
 }
