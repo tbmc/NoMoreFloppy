a7fac046970aee0f6dae14fae78063e308fe9e91
tbmc <me@tbmc.fr>
2014-09-14 15:01:39 +0200

    ENFOIRE DE GIT DE MERDE!!!

--- Controller.cpp
+++ Controller.cpp
@@ -1,24 +1,27 @@
 #include "Controller.h"
-
 using namespace sf;
-
+using namespace std;
+Fichier::~Fichier()
+{
+    delete file;
+    delete name;
+    delete path;
+}
 Controller::Controller()
 {
 }
-
 Controller::~Controller()
 {
+    delete fichier;
 }
-
-STATUT Controller::connect(const char *ip, uShort port)
+STATUS Controller::connect(const char *ip, uShort port, uInt32 timeout)
 {
     socket.disconnect();
-    if(socket.connect(ip, port) != Socket::Done)
+    if(socket.connect(ip, port, milliseconds(timeout)) != Socket::Done)
         return Error_socket;
     return Ok;
 }
-
-STATUT Controller::serverWaitConnection(uShort port)
+STATUS Controller::serverWaitConnection(uShort port)
 {
     socket.disconnect();
     TcpListener listener;
@@ -28,13 +31,93 @@ STATUT Controller::serverWaitConnection(uShort port)
         return Error_socket;
     return Ok;
 }
-
 void Controller::disconnect()
 {
     socket.disconnect();
 }
-
-STATUT Controller::sendFile(const char *file, uInt32 packetSize, int compression_level)
+STATUS Controller::sendFileRequest(const char *path)
+{
+    // Stockage du nom, du chemin et de la taille du fichier
+    if(fichier == NULL)
+        delete fichier;
+    fichier = new Fichier;
+    fichier->file = fopen(path, "rb");
+    if(fichier->file == NULL)
+        return Error_file;
+    fseek(fichier->file, 0, SEEK_END);
+    fichier->filesize = ftell(fichier->file);
+    fclose(fichier->file);
+    fichier->path = new char(strlen(path) + 1);
+    strcpy(fichier->path, path);
+    const char *na = strrchr(path, '/');
+    if(na == NULL)
+        na = path;
+    else
+        na++;
+    fichier->name = new char(strlen(na) + 1);
+    strcpy(fichier->name, na);
+    // Envoie de la demande
+    ChristmasPacket paquet;
+    paquet << fichier->filesize << string(fichier->name);
+    Socket::Status statsend;
+    statsend = socket.send(paquet);
+    if(statsend != Socket::Done)
+        return Error_socket;
+    // Réception de la réponse
+    SocketSelector selector;
+    selector.add(socket);
+    if(!selector.wait(milliseconds(DEFAULT_TIMEOUT)))
+        return Error_socket;
+    paquet.clear();
+    statsend = socket.receive(paquet);
+    if(statsend != Socket::Done)
+        return Error_socket;
+    bool accept;
+    paquet >> accept;
+    if(accept)
+        return Ok;
+    else
+        return File_refused;
+}
+STATUS Controller::receiveFileRequest(const char *folderpath, Answer &answer)
+{
+    Socket::Status stat;
+    ChristmasPacket paquet;
+    // Réception de la demande
+    stat = socket.receive(paquet);
+    if(stat != Socket::Done)
+        return Error_socket;
+    if(fichier == NULL)
+        delete fichier;
+    // Parsing de la demande
+    string name, chemin("");
+    uInt32 filesize;
+    paquet >> filesize;
+    paquet >> name;
+    // Demande si l'utilisateur veut recevoir le fichier
+    bool response = answer.acceptFile(name.c_str(), filesize);
+    // Envoie de la réponse de l'utilisateur
+    paquet.clear();
+    paquet << response;
+    if(!response)
+        return File_refused;
+    stat = socket.send(paquet);
+    if(stat != Socket::Done)
+        return Error_socket;
+    // Stockage du fichier à recevoir
+    fichier = new Fichier;
+    fichier->filesize = filesize;
+    fichier->name = new char(name.length() + 1);
+    strcpy(fichier->name, name.c_str());
+    chemin = folderpath;
+    if(chemin[chemin.length() - 1] != '/')
+        chemin.push_back('/');
+    chemin.append(name);
+    fichier->path = new char(chemin.length() + 1);
+    strcpy(fichier->path, chemin.c_str());
+    return Ok;
+}
+STATUS Controller::sendFile(const char *file, uInt32 packetSize, int compression_level)
 {
     if(compress.setCompressionLevel(compression_level) != NP)
         return Error;
@@ -67,11 +150,9 @@ STATUT Controller::sendFile(const char *file, uInt32 packetSize, int compression
         printf("Erreur premier paquet!\n");
         return Error_socket;
     }
-
     char *temp = new char(packetSize * 5);
     message.data = new char(packetSize * 5);
     message.type = PACKET_DATA;
-
     while((readSize = fread(temp, 1, packetSize, f)) > 0)
     {
         printf("%ld %ld\n", readSize, packetSize);
@@ -92,13 +173,11 @@ STATUT Controller::sendFile(const char *file, uInt32 packetSize, int compression
             break;
         }
     }
-
     packet.clear();
     message.type = PACKET_END;
     message.dataSize = 0;
     packet.appendData(message);
     socket.send(packet);
-
     delete temp;
     delete message.data;
     fclose(f);
@@ -109,8 +188,7 @@ STATUT Controller::sendFile(const char *file, uInt32 packetSize, int compression
     printf("NICE!\n");
     return Ok;
 }
-
-STATUT Controller::receiveFile(const char *folderpath, uInt32 packetSize)
+STATUS Controller::receiveFile(const char *folderpath, uInt32 packetSize)
 {
     FILE * f = NULL;
     COMP_S outComp;
@@ -127,15 +205,17 @@ STATUT Controller::receiveFile(const char *folderpath, uInt32 packetSize)
     Message message;
     message.data = new char(packetSize * 2);
     packet.readData(message);
-    if(message.data[message.dataSize - 1] != '/')
-    {
-        message.data[message.dataSize] = '/';
-        message.dataSize++;
-        message.data[message.dataSize] = '\0';
-    }
     name = new char(message.dataSize + strlen(folderpath) + 10);
+    name[0] = '\0';
     strcat(name, folderpath);
+    int len = strlen(name);
+    if(name[len - 1] != '/')
+    {
+        name[len] = '/';
+        name[len+1] = '\0';
+    }
     strcat(name, message.data);
+    printf("FICHIER : '%s'\n", name);
     f = fopen(name, "wb");
     if(f == NULL)
     {
@@ -145,7 +225,6 @@ STATUT Controller::receiveFile(const char *folderpath, uInt32 packetSize)
         printf("File == NULL!\n");
         return Error_file;
     }
-
     do
     {
         status = socket.receive(packet);
@@ -167,7 +246,6 @@ STATUT Controller::receiveFile(const char *folderpath, uInt32 packetSize)
             fwrite(temp, 1, outSize, f);
         }
     }while(message.type != PACKET_END);
-
     delete temp;
     delete message.data;
     delete name;
@@ -178,35 +256,3 @@ STATUT Controller::receiveFile(const char *folderpath, uInt32 packetSize)
     printf("NICE!\n");
     return Ok;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Controller.h
+++ Controller.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <string>
 #include "compress.h"
 #include "ChristmasPacket.h"
 
@@ -16,7 +16,7 @@
 
 #define DEFAULT_PORT (25666)
 #define DEFAULT_IP "127.0.0.1"
-
+#define DEFAULT_TIMEOUT (5000)
 #define PACKET_DATA (0)
 #define PACKET_INFORMATION (1)
 #define PACKET_FUCKER (3)
@@ -25,39 +25,89 @@
 typedef unsigned short uShort;
 typedef uint32_t uInt32;
 //#define MAX_PACKET_SIZE (67108864)
+
 #define MAX_PACKET_SIZE (1024)
 #define DEFAULT_PACKET_SIZE (1048576)
 
-enum STATUT
+enum STATUS
 {
-    Ok,
+    Ok, // Si aucune erreur ne s'est produite
     Error,
     Error_port,
     Error_socket,
     Error_bufferoverflow,
-    Error_file
+    Error_file,
+    File_refused
+};
+
+class Fichier {
+public:
+    FILE * file;
+    uInt32 filesize;
+    char * name;
+    char * path;
+    ~Fichier();
+};
+
+class Answer
+{
+public:
+    virtual bool acceptFile(const char *filename, uInt32 filesize) = 0;
 };
 
 class Controller {
-
 private:
     sf::TcpSocket socket;
     Compress compress;
-
+    Fichier *fichier = NULL;
 public:
     Controller();
     ~Controller();
-
     // Contrôle de la connection
-    STATUT connect(const char *ip, uShort port = DEFAULT_PORT);
-    STATUT serverWaitConnection(uShort port = DEFAULT_PORT);
+    STATUS connect(const char *ip, uShort port = DEFAULT_PORT, uInt32 timeout = DEFAULT_TIMEOUT);
+    STATUS serverWaitConnection(uShort port = DEFAULT_PORT);
     void disconnect();
-
+    //Demande d'envoie de fichier
+    STATUS sendFileRequest(const char *path);
+    STATUS receiveFileRequest(const char *folderpath, Answer &answer);
     //Envoie et réception de fichier
-    STATUT sendFile(const char *file, uInt32 packetSize = DEFAULT_PACKET_SIZE,
+    STATUS sendFile(const char *file, uInt32 packetSize = DEFAULT_PACKET_SIZE,
                     int compression_level = DEFAULT_COMPRESSION_LEVEL);
-    STATUT receiveFile(const char *folderpath, uInt32 packetSize = DEFAULT_PACKET_SIZE);
+    STATUS receiveFile(const char *folderpath, uInt32 packetSize = DEFAULT_PACKET_SIZE);
+
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif // CONTROLLER_H
